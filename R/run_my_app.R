4673d3d1d43c5015ee1d48166b8c86637918b12a
Auburngrads <auburngrads@live.com>
2018-02-05 14:21:30 -0500

    Update R/run_my_app.R

--- R/run_my_app.R
+++ R/run_my_app.R
@@ -1,3 +1,20 @@
+#' @title Runs a shiny app in my package
+#'
+#' @description
+#' Runs as shiny app
+#'
+#' @param app_name Charater string for a directory in this package
+#' @param ...      Additional options passed to shinyAppDir
+#'
+#' @return A printed shiny app
+#'
+#'
+#' @importFrom shiny shinyAppDir
+#' @import dplyr
+#' @examples
+#' \dontrun{ run_my_app('myfirstApp')}
+#'
+#' @export
 run_my_app <- function(app_name,...) {
 
   app_dir <- system.file('apps', app_name, package = 'OPER782')
